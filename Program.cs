fe44d6495bf904a1868e3ccd1c1821357373821b
Oleg <StavSonySony@gmail.com>
2021-09-30 11:45:10 +0300

    rrr

--- Program.cs
+++ Program.cs
@@ -8,8 +8,16 @@ namespace dz4
         {
             Console.WriteLine("Hello World!");
             SchoolClass classs = new SchoolClass(5);
-            classs[0] = new Learner {Name = "Ivan", LastName = "Ivanov", MiddleName = "Ivanovich", Years = 20};
+            classs[0] = new Learner {Name = "Ivan", LastName = "Ivanov1", MiddleName = "Ivanovich1", Years = 21};
+            classs[1] = new Learner {Name = "Ivan", LastName = "Ivanov2", MiddleName = "Ivanovich2", Years = 22};
+            classs[2] = new Learner {Name = "Ivan", LastName = "Ivanov3", MiddleName = "Ivanovich3", Years = 23};
+            classs[3] = new Learner {Name = "Olga", LastName = "Sidorova", MiddleName = "Victorovna", Years = 20};
+            classs[4] = new Learner {Name = "Petya", LastName = "Petrov", MiddleName = "Ivanovich", Years = 25};
+
+         
+
             Console.WriteLine(classs[0]);
+            Console.WriteLine(classs["Ivan"]);
 
         }
     }
--- SchoolClass.cs
+++ SchoolClass.cs
@@ -4,12 +4,41 @@ namespace dz4
 {
     public class SchoolClass
     {
-        public Learner [] Room { get; set; }
+        public Learner [] Room {set;get;}
         
-        
-        
+        public SchoolClass (int coll)
+        {
+            Room = new Learner [coll];
+        }
+        public Learner this[int index] 
+        {
+            get 
+            {
+                return Room [index];
+            }
+            set
+            {
+                Room [index] = value;
+            }
+        }
+        public Learner this[string  name] 
+        {
+           get 
+            {
+
+                foreach (var item in Room)
+                { 
+                    if(item.Name == name)
+                    return item ;
+                }
+            }
+           // set
+           // {
+           //     Room ["name"] = value;
+           // }
+        }
+
+
+
     }
-
-
-
 }
